d513274b5bb67c8f0ebc815d5d7a003613f638a0
Luiz Soares <dev.soares@outlook.com>
2019-04-09 14:54:41 -0300

    script de categoria

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
   const express = require('express')
   const handlebars =  require('express-handlebars')
   const bodyParser = require('body-parser')
-  //const mongoose = require('mongoose')
+  const mongoose = require('mongoose')
   const app = express()
   const admin = require('./routes/admin')
   const path = require('path')
@@ -14,7 +14,12 @@
     app.engine('handlebars', handlebars({deafaultLayout:'main'}))
     app.set('view engine', 'handlebars')
   // mongoose
-    //em breve
+    mongoose.Promise = global.Promise
+    mongoose.connect('mongodb://localhost/blogapp', {useNewUrlParser: true}).then(()=>{
+      console.log('Conectado ao mongo')
+    }).catch((err)=>{
+      console.log('Erro ao se conectar: '+err)
+    })
   // Public
     app.use(express.static(path.join(__dirname, 'public')))
 //ROTAS
