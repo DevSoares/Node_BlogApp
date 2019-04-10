532f86255fc032022a8d18edbb89cee80f0da6d8
Luiz Soares <dev.soares@outlook.com>
2019-04-10 14:37:05 -0300

    formulário de criação de postagem concluído

--- routes/admin.js
+++ routes/admin.js
@@ -104,4 +104,17 @@ router.post('/categorias/deletar', (req, res)=>{
   })
 })
 
+router.get('/postagens', (req, res)=>{
+  res.render('admin/postagens')
+})
+
+router.get('/postagens/add', (req, res)=>{
+  Categoria.find().then((categorias)=>{
+    res.render('admin/addpostagens', {categorias:categorias})
+  }).catch((err)=>{
+    req.flash('error_msg', 'Houve um erro ao carregar o formulário')
+    res.redirect('/admin')
+  })
+})
+
 module.exports = router
