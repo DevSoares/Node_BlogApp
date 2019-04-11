2a8486d149170a594a7af39e5f4e4edc05cf82fa
Luiz Soares <dev.soares@outlook.com>
2019-04-11 14:48:07 -0300

    navbar atualizado

--- app.js
+++ app.js
@@ -10,6 +10,8 @@
   const flash = require('connect-flash')
   require('./models/Postagem')
   const Postagem = mongoose.model('postagens')
+  require('./models/Categoria')
+  const Categoria = mongoose.model('categorias')
 //CONFIGURACOES
   // Sessão
     app.use(session({
@@ -49,6 +51,48 @@
       res.redirect('/404')
     })
 
+  })
+
+  app.get('/postagem/:slug',(req,res)=>{
+    Postagem.findOne({slug: req.params.slug}).then((postagem)=>{
+      if(postagem){
+        res.render('postagem/index',{postagem:postagem})
+      }else{
+        req.flash('error_msg', 'Esta postagem não existe!')
+        res.redirect('/')
+      }
+    }).catch((err)=>{
+      req.flash('error_msg', 'Houve um erro interno.')
+      res.redirect('/')
+    })
+  })
+
+  app.get('/categorias', (req,res)=>{
+    Categoria.find().then((categorias)=>{
+      res.render('categorias/index', {categorias:categorias})
+    }).catch((err)=>{
+      req.flash('error_msg','Houve um erro interno ao listar as categorias.')
+      res.redirect('/')
+    })
+  })
+
+  app.get('/categorias/:slug', (req,res)=>{
+    Categoria.findOne({slug:req.params.slug}).then((categoria)=>{
+      if(categoria){
+        Postagem.find({categoria:categoria._id}).then((postagens)=>{
+          res.render('categorias/postagens',{postagens:postagens,categoria:categoria})
+        }).catch((err)=>{
+          req.flash('error_msg', 'Houve um erro ao listar os posts!')
+          res.redirect('/')
+        })
+      }else {
+        req.flash('error_msg', 'Esta categoria não existe.')
+        res.redirect('/')
+      }
+    }).catch((err)=>{
+      req.flash('error_msg','Erro ao carregar a página desta categoria.')
+      res.redirect('/')
+    })
   })
 
   app.get('/404', (req,res)=>{
