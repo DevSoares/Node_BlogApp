75b5b2c74b3a3ccc60f4a73cfcc004542a4776d1
Luiz Soares <dev.soares@outlook.com>
2019-04-11 16:58:22 -0300

    sistema de login finalizado

--- app.js
+++ app.js
@@ -12,6 +12,9 @@
   const Postagem = mongoose.model('postagens')
   require('./models/Categoria')
   const Categoria = mongoose.model('categorias')
+  const usuarios = require('./routes/usuario')
+  const passport = require('passport')
+  require('./config/auth')(passport)
 //CONFIGURACOES
   // Sessão
     app.use(session({
@@ -19,11 +22,14 @@
       resave: true,
       saveUninitialized: true
     }))
+    app.use(passport.initialize())
+    app.use(passport.session())
     app.use(flash())
   // Middleware
     app.use((req, res, next)=>{
       res.locals.success_msg = req.flash('success_msg')
       res.locals.error_msg = req.flash('error_msg')
+      res.locals.error = req.flash('error')
       next()
     })
   // bodyParser
@@ -100,6 +106,7 @@
   })
 
   app.use('/admin', admin)
+  app.use('/usuarios', usuarios)
 //OUTROS
 const port = 8093
 app.listen(port,()=>{
